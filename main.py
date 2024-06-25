2a3af5b25362df4ea845cd42c35561784d2db560
(Ariel <ariel.calzada@gmail.com>
2024-06-25 17:19:11 -0500

    Add turn on 10 sec

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import time
+
+import RPi.GPIO as GPIO
+
+
 def main():
-    print("Hello world!")
+    # Set up GPIO using BCM numbering
+    GPIO.setmode(GPIO.BCM)
+
+    # Set up GPIO pin 18 as an output
+    GPIO.setup(18, GPIO.OUT)
+
+    try:
+        # Turn on the LED
+        GPIO.output(18, GPIO.HIGH)
+        print("LED is ON")
+
+        # Keep the LED on for 10 seconds
+        time.sleep(10)
+
+        # Turn off the LED
+        GPIO.output(18, GPIO.LOW)
+        print("LED is OFF")
+
+    finally:
+        # Clean up GPIO settings
+        GPIO.cleanup()
 
 
 if __name__ == "__main__":
