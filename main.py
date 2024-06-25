19ac2b14419a8ced8071b86c72c3b9ca6cd0f97c
(Ariel <ariel.calzada@gmail.com>
2024-06-25 18:55:55 -0500

    Add change state

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main() -> None:
-    print("Hello World!")
+from fastapi import FastAPI, Request
+import RPi.GPIO as GPIO
+
+from libs.models import InputMessage, OutputMessage
+from libs.raspberry.led_handler import LEDHandler
+
+app = FastAPI()
+
+
+@app.post("/state")
+def change_state(
+    request: Request, input_message: InputMessage
+) -> OutputMessage:
+    state = input_message.state
+
+    handler = LEDHandler()
+    if state:
+        handler.on()
+    else:
+        handler.off()
+    GPIO.cleanup()
+
+    output_message = OutputMessage(state=state)
+
+    return output_message
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    uvicorn.run(app="main:app", host="0.0.0.0", port=9600, reload=True)
