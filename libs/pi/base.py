2c98eaef7f30ed1cd5683507f0f41fa06d5c55e3
(Ariel <ariel.calzada@gmail.com>
2024-06-27 13:34:15 -0500

    Add warning off

--- libs/pi/base.py
+++ libs/pi/base.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 class RaspberryBaseHandler:
     def __init__(self) -> None:
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
     @staticmethod
     def cleanup():
